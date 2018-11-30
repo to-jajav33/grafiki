d83db059a967420ef3670d8cae536dfcfe5cdc12
jajav33 <to.jajav33@gmail.com>
2018-11-30 09:09:11 -0800

    Initial working JSON graph converter, I think.

---
+++ src/Grafiki.ts
@@ -0,0 +1,75 @@
+
+import { GNodeRoot } from "./components/GNodeRoot";
+import * as MyWebSocket from 'ws';
+import { IGNodeOptions } from "./components/GNode";
+
+interface IGrafikiParams {
+	port ?: number,
+	nodeOptions : IGNodeOptions 
+}
+
+interface IGrafikiOptions {
+	port : number
+}
+
+
+export class Grafiki extends GNodeRoot {
+	private __wsServer : MyWebSocket.Server;
+	protected __grafikiOptions : IGrafikiOptions
+
+	constructor (paramOptions : IGrafikiParams = {nodeOptions : {root: undefined}}) {
+		paramOptions.nodeOptions.root = undefined;
+
+		super(paramOptions.nodeOptions);
+
+		this.__options.root = this;
+		this.__grafikiOptions = {
+			port: paramOptions.port
+		};
+
+		this.__init();
+	}
+
+	private __init () {
+		if (this.__wsServer) return;
+
+		let port : number = this.__grafikiOptions.port;
+
+		try {
+			if (port) {
+				this.__onConnection = this.__onConnection.bind(this);
+				this.__onMessage = this.__onMessage.bind(this);
+
+				this.__wsServer = new MyWebSocket.Server({port});
+	
+				this.__wsServer.on('connection', this.__onConnection);
+			}
+		} catch (e) {
+			return e;
+		}
+	}
+
+	/**
+	 * Listern for then server connectss.
+	 *
+	 * @private
+	 * @param {*} paramWs
+	 * @memberof Grafiki
+	 */
+	private __onConnection (paramWs) {
+		paramWs.on('message', this.__onMessage);
+	}
+
+	/**
+	 * Listener for when a message is received
+	 *
+	 * @private
+	 * @param {*} paramMessage
+	 * @memberof Grafiki
+	 */
+	private __onMessage (paramMessage) {
+		// console.log('received:', paramMessage);
+	}
+}
+
+
