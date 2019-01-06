366044a0d150f4c6f8616c21bfa665986cb1f9e3
jajav33 <to.jajav33@gmail.com>
2019-01-06 11:31:28 -0800

    Refactored testing for testing different scenarios for getData.
    break: Breaking change in getData. No only returns first level.
    
    fyi: Will require to pass in a query for more than 1st level in getData

--- test/test.ts
+++ test/test.ts
@@ -1,7 +1,44 @@
 import { Grafiki } from "../src/Grafiki";
 
+const TEST_PATH_MODES = {
+	ROOT: 'ROOT',
+	ONE_LEVEL: 'ONE_LEVEL',
+	TWO_LEVELS: 'TWO_LEVEL',
+	THREE_LEVELS_MULTI_ATTR: 'THREE_LEVEL',
+	THREE_LEVELS_SINGLE_ATTR: 'THREE_LEVEL_SINGLE_ATTR'
+}
+
 class MyTest {
 	constructor() {
+	}
+
+	async _addData (paramGNode) {
+		let doggyRef = await paramGNode.ref('/dogs/doggie');
+		let jajav33Ref = await paramGNode.ref('/people/jajav33');
+
+		await doggyRef.put({
+			name: 'doggie',
+			isAwesome: true,
+			age: 18,
+			owners: {
+				'jajav33': jajav33Ref
+			}
+		});
+
+		await jajav33Ref.put({
+			name: 'jajav33',
+			isAwesome: true,
+			age: 31,
+			pets: {
+				'doggie': doggyRef
+			},
+			'3': 3,
+			'1': 7,
+			'7': 3,
+			'0': 1,
+		});
+
+		return {jajav33Ref, doggyRef};
 	}
 
 	async runTests() {
@@ -9,14 +46,65 @@ class MyTest {
 			// let result = await this.testInitGrafiki();
 			// let result = await this.getInNode();
 			// let result = await this.putInNode();
-			// let result = await this.getDataFromNode();
 
-			let useTemplateLiteral = true;
-			// let result = await this.getDataFromNode(useTemplateLiteral);
+			let useTemplateLiteral;
+			let shouldReturnPath; // either return path with value, or just value
+			let testPathMode;
+			// useTemplateLiteral = false;
 
-			// let result = await this.perisistentDataPut();
-			let result = await this.perisistentDataRead(useTemplateLiteral);
+			// shouldReturnPath = false; 
+			// testPathMode = TEST_PATH_MODES.ROOT;
+			// let result = await this.getDataFromNode({useTemplateLiteral, shouldReturnPath, testPathMode});
+			// testPathMode = TEST_PATH_MODES.ONE_LEVEL;
+			// let result = await this.getDataFromNode({useTemplateLiteral, shouldReturnPath, testPathMode});
+			// testPathMode = TEST_PATH_MODES.TWO_LEVELS;
+			// let result = await this.getDataFromNode({useTemplateLiteral, shouldReturnPath, testPathMode});
+			// testPathMode = TEST_PATH_MODES.THREE_LEVELS_MULTI_ATTR;
+			// let result = await this.getDataFromNode({useTemplateLiteral, shouldReturnPath, testPathMode});
+			// testPathMode = TEST_PATH_MODES.THREE_LEVELS_SINGLE_ATTR;
+			// let result = await this.getDataFromNode({useTemplateLiteral, shouldReturnPath, testPathMode});
+
+			// shouldReturnPath = true;
+			// testPathMode = TEST_PATH_MODES.ROOT;
+			// let result = await this.getDataFromNode({useTemplateLiteral, shouldReturnPath, testPathMode});
+			testPathMode = TEST_PATH_MODES.ONE_LEVEL;
+			let result = await this.getDataFromNode({useTemplateLiteral, shouldReturnPath, testPathMode});
+			// testPathMode = TEST_PATH_MODES.TWO_LEVELS;
+			// let result = await this.getDataFromNode({useTemplateLiteral, shouldReturnPath, testPathMode});
+			// testPathMode = TEST_PATH_MODES.THREE_LEVELS_MULTI_ATTR;
+			// let result = await this.getDataFromNode({useTemplateLiteral, shouldReturnPath, testPathMode});
+			// testPathMode = TEST_PATH_MODES.THREE_LEVELS_SINGLE_ATTR;
+			// let result = await this.getDataFromNode({useTemplateLiteral, shouldReturnPath, testPathMode});
+
+			useTemplateLiteral = true;
+
+			// shouldReturnPath = false;
+			// testPathMode = TEST_PATH_MODES.ROOT;
+			// let result = await this.getDataFromNode({useTemplateLiteral, shouldReturnPath, testPathMode});
+			// testPathMode = TEST_PATH_MODES.ONE_LEVEL;
+			// let result = await this.getDataFromNode({useTemplateLiteral, shouldReturnPath, testPathMode});
+			// testPathMode = TEST_PATH_MODES.TWO_LEVELS;
+			// let result = await this.getDataFromNode({useTemplateLiteral, shouldReturnPath, testPathMode});
+			// testPathMode = TEST_PATH_MODES.THREE_LEVELS_MULTI_ATTR;
+			// let result = await this.getDataFromNode({useTemplateLiteral, shouldReturnPath, testPathMode});
+			// testPathMode = TEST_PATH_MODES.THREE_LEVELS_SINGLE_ATTR;
+			// let result = await this.getDataFromNode({useTemplateLiteral, shouldReturnPath, testPathMode});
+
+			shouldReturnPath = true;
+			// testPathMode = TEST_PATH_MODES.ROOT;
+			// let result = await this.getDataFromNode({useTemplateLiteral, shouldReturnPath, testPathMode});
+			// testPathMode = TEST_PATH_MODES.ONE_LEVEL;
+			// let result = await this.getDataFromNode({useTemplateLiteral, shouldReturnPath, testPathMode});
+			// testPathMode = TEST_PATH_MODES.TWO_LEVELS;
+			// let result = await this.getDataFromNode({useTemplateLiteral, shouldReturnPath, testPathMode});
+			// testPathMode = TEST_PATH_MODES.THREE_LEVELS_MULTI_ATTR;
+			// let result = await this.getDataFromNode({useTemplateLiteral, shouldReturnPath, testPathMode});
+			// testPathMode = TEST_PATH_MODES.THREE_LEVELS_SINGLE_ATTR;
+			// let result = await this.getDataFromNode({useTemplateLiteral, shouldReturnPath, testPathMode});
+
 			// let result = await this.perisistentDataClear();
+			// let result = await this.perisistentDataPut();
+			// let result = await this.perisistentDataRead(useTemplateLiteral);
 
 			return result;
 		} catch (e) {
@@ -33,46 +121,82 @@ class MyTest {
 		return new Grafiki();
 	}
 
-	async getDataFromNode(useTemplateLiteral?: boolean) {
+	async getDataFromNode(options) {
 		try {
+			options = options || {};
+			const {useTemplateLiteral, shouldReturnPath, testPathMode} = options;
 
 			let g = new Grafiki();
-			let doggyRef = await g.ref('/dogs/doggie');
-			let jajav33Ref = await g.ref('/people/jajav33');
-	
-			await doggyRef.put({
-				name: 'doggie',
-				isAwesome: true,
-				age: 18,
-				owners: {
-					'jajav33': jajav33Ref
-				}
-			});
-	
-			await jajav33Ref.put({
-				name: 'jajav33',
-				isAwesome: true,
-				age: 31,
-				pets: {
-					'doggie': doggyRef
-				},
-				'3': 3,
-				'1': 7,
-				'7': 3,
-				'0': 1,
-			});
-	
-			// let pathQuery = '/pets';
-			let pathQuery = '/';
-			if (useTemplateLiteral) {
-				pathQuery = `{
-					pets: {}
-				}`
+			let {jajav33Ref} = await this._addData(g);
+
+			let pathQuery;
+			switch (testPathMode) {
+				case TEST_PATH_MODES.ROOT:
+					pathQuery = '/';
+					if (useTemplateLiteral) {
+						pathQuery = '{}';
+					}
+					break;
+				case TEST_PATH_MODES.ONE_LEVEL:
+					pathQuery = '/pets';
+					if (useTemplateLiteral) {
+						pathQuery = `{
+							pets: {}
+						}`;
+					}
+					break;
+				case TEST_PATH_MODES.TWO_LEVELS:
+					pathQuery = '/pets/doggie';
+					if (useTemplateLiteral) {
+						pathQuery = `{
+							pets: {
+								doggie: {}
+							}
+						}`;
+					}
+					break;
+				case TEST_PATH_MODES.THREE_LEVELS_MULTI_ATTR:
+					pathQuery = [
+						'/pets/doggie/name',
+						'/pets/doggie/age',
+						'/pets/doggie/owners'
+					];
+					if (useTemplateLiteral) {
+						pathQuery = `{
+							pets: {
+								doggie: {
+									name: {},
+									age: {},
+									owners: {}
+								}
+							}
+						}`;
+					}
+					break;
+				case TEST_PATH_MODES.THREE_LEVELS_SINGLE_ATTR:
+				default:
+					pathQuery = [
+						['/pets/doggie/age'],
+					];
+					if (useTemplateLiteral) {
+						pathQuery = `{
+							pets: {
+								doggie: {
+									age: {}
+								}
+							}
+						}`;
+					}
+					break;
 			}
-	
+
 			let result = await jajav33Ref.getData(pathQuery);
 	
-			return result;
+			// if you want the entire path with the value from ref use result.path()
+			if (shouldReturnPath) {
+				return result.path();
+			}
+			return result.val();
 		} catch (e) {
 			throw e;
 		}
